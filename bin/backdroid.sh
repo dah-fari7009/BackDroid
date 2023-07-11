b08d4ed2574f100b08553681ecd57c1cc9fd8a7e
Faridah Akinotcho <fari100@zeus.ece.ubc.ca>
2023-07-10 17:14:58 -0700

    for merge conflicts

--- bin/backdroid.sh
+++ bin/backdroid.sh
@@ -2,9 +2,9 @@
 
 mainClass=../lib/classes-2.3.4_r1.jar
 mainGAPI=../lib/gapi-16.jar
-mainSDK=../lib/android_v25.jar
+mainSDK=../lib/android_v30.jar
 
-otherClass=../lib/android_v28.jar:../lib/android_v27.jar:../lib/android_v23.jar:../lib/android_v22.jar:../lib/android_v19.jar:../lib/android_v18.jar:../lib/android_v15.jar:../lib/android_v13.jar:../lib/android_v10.jar:../lib/android_v8.jar:../lib/android_v7.jar:../lib/android_v4.jar:../lib/classes-2.2_r1.jar
+otherClass=../lib/android_v30.jar:../lib/android_v28.jar:../lib/android_v27.jar:../lib/android_v23.jar:../lib/android_v22.jar:../lib/android_v19.jar:../lib/android_v18.jar:../lib/android_v15.jar:../lib/android_v13.jar:../lib/android_v10.jar:../lib/android_v8.jar:../lib/android_v7.jar:../lib/android_v4.jar:../lib/classes-2.2_r1.jar
 otherSDK=../lib/android-support-v13.jar:../lib/android-support-v7-appcompat.jar:../lib/android-support-v7-cardview.jar:../lib/android-support-v7-gridlayout.jar:../lib/android-support-v7-mediarouter.jar:../lib/android-support-v7-palette.jar:../lib/android-support-v7-preference.jar:../lib/android-support-v7-recyclerview.jar:../lib/android-support-v4.jar
 
 infoflow=../lib/soot-infoflow.jar:../lib/soot-infoflow-android.jar
